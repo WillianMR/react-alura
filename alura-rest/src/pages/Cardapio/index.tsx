3aea8b4a03b57c351532646254ec904472270382
WillianMR <58883121+WillianMR@users.noreply.github.com>
2022-06-28 09:09:17 -0300

    Projeto Restaurante

---
+++ alura-rest/src/pages/Cardapio/index.tsx
@@ -0,0 +1,17 @@
+import styles from './Cardapio.module.scss'
+import {ReactComponent as Logo} from "assets/logo.svg"
+
+export default function Cardapio () {
+    return (
+        <main>
+             <nav className={styles.menu}>
+                <Logo /> 
+            </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>Casa do Código e da Massa
+
+                </div>
+            </header>
+        </main>
+    )
+}
