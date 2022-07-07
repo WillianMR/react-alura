d92269e1fbf8bf5e6036501fe7ae71888c676054
WillianMR <58883121+WillianMR@users.noreply.github.com>
2022-07-07 14:37:40 -0300

    refatorando em Hooks

---
+++ event-tracker-master/src/state/atom.ts
@@ -0,0 +1,20 @@
+import { atom } from "recoil";
+import { IEvento } from "../interfaces/IEvento";
+
+export const listaDeEventosState = atom<IEvento[]>({
+  key: 'listaDeEventosState',
+  default: [{
+    "descricao": "Estudar React",
+    "inicio": new Date("2022-07-08T09:00"),
+    "fim": new Date("2022-07-08T13:00"),
+    "completo": false,
+    "id": 1642342747
+  },
+  {
+    "descricao": "Estudar Recoil",
+    "inicio": new Date("2022-07-09T09:00"),
+    "fim": new Date("2022-07-09T11:00"),
+    "completo": false,
+    "id": 1642342959
+  }]
+})
