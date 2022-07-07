3f8d3ba2f5f21be244e8cab601115208d25e1866
WillianMR <58883121+WillianMR@users.noreply.github.com>
2022-07-07 14:56:33 -0300

    Atualizando Filtros

--- event-tracker-master/src/App.tsx
+++ event-tracker-master/src/App.tsx
@@ -45,9 +45,9 @@ function App() {
   //   // setEventos([...eventos.filter(evento => evento.id !== id)])
   // }
 
-  const aplicarFiltro = (data: Date | null) => {
-    setFiltro(data)
-  }
+  // const aplicarFiltro = (data: Date | null) => {
+  //   setFiltro(data)
+  // }
 
   // const filtrados = !filtro
   //   ? eventos
@@ -64,9 +64,7 @@ function App() {
           </Card>
           <hr />
           <Card>
-            <ListaDeEventos
-              aoFiltroAplicado={aplicarFiltro}
-              />
+            <ListaDeEventos/>
           </Card>
         </div>
         <div className={style.Coluna}>
